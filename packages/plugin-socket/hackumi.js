66fdd20e3046dc3a6f4b559d5b0f12824fd4c142
xiaohuoni <xiaohuoni@dingtalk.com>
2023-03-09 15:11:27 +0800

    fix: Repeat write operation

--- packages/plugin-socket/hackumi.js
+++ packages/plugin-socket/hackumi.js
@@ -4,10 +4,15 @@ const { writeFileSync, readFileSync } = require('fs');
 try {
     const target = join(dirname(require.resolve('@umijs/bundler-webpack/package.json')), 'dist/server/server.js');
     const server = readFileSync(target, 'utf-8');
+    if (server.includes('global.g_umi_ws')) {
+        console.log("umi 已经拥有全局对象 global.g_umi_ws，无需重复覆盖");
+        return;
+    }
+    console.log(target);
     // 先本地覆盖，后续等确定了用法，再从 umi 开接口
-    server.replace('ws.wss.on("connection"', 'global.g_umi_ws = ws;\nws.wss.on("connection"');
-    writeFileSync(target, server, 'utf-8')
-    console.log("覆盖 umi 文件成功，生成全局对象 global.g_umi_ws。")
+    const s = server.replace('ws.wss.on("connection"', 'global.g_umi_ws = ws;\nws.wss.on("connection"');
+    writeFileSync(target, s, 'utf-8');
+    console.log("覆盖 umi 文件成功，生成全局对象 global.g_umi_ws。");
 } catch (error) {
-    console.log("覆盖 umi 文件失败，Mongchhi Socket 相关的功能无法使用。")
+    console.log("覆盖 umi 文件失败，Mongchhi Socket 相关的功能无法使用。");
 }
