ef94208a2a9cf48f11ccd5abcfe4fb98a123ade0
plus <ljw@live.jp>
2023-03-10 13:32:51 +0800

    fix: typo

--- packages/plugin-mongchhi/src/index.ts
+++ packages/plugin-mongchhi/src/index.ts
@@ -3,7 +3,7 @@ import getUmiAppData from './getUmiAppData';
 
 export default (api: IApi) => {
   api.onStart(() => {
-    if (['dev']).includes(api.name) === true) {
+    if (['dev'].includes(api.name) === true) {
       getUmiAppData();
     }
   });
