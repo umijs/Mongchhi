1f47f61f5d6df3793ab60e6195449f77ee6f26c5
plus <ljw@live.jp>
2023-03-10 13:44:35 +0800

    feat: get appdata

--- packages/plugin-mongchhi/src/index.ts
+++ packages/plugin-mongchhi/src/index.ts
@@ -1,10 +1,19 @@
 import { IApi } from '@mongchhi/types';
+import { localUmiAppData, type IAppData } from '@mongchhi/utils';
 import getUmiAppData from './getUmiAppData';
 
 export default (api: IApi) => {
   api.onStart(() => {
-    if (['dev'].includes(api.name) === true) {
+    // @ts-ignore
+    if (api.service.opts.frameworkName === 'mongchhi') {
       getUmiAppData();
     }
+  });
+
+  api.onDevCompileDone(() => {
+    localUmiAppData.update((appData: IAppData) => ({
+      ...appData,
+      [api.appData.cwd]: api.appData,
+    }));
   });
 };
