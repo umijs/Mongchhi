baa7755336ca3fb18e5cfe9d3f220bc8ab340ec1
plus <ljw@live.jp>
2023-03-10 14:02:27 +0800

    feat: comment

--- packages/plugin-mongchhi/src/index.ts
+++ packages/plugin-mongchhi/src/index.ts
@@ -6,11 +6,13 @@ export default (api: IApi) => {
   api.onStart(() => {
     // @ts-ignore
     if (api.service.opts.frameworkName === 'mongchhi') {
+      // mongchhi 主程序，回去全部 appData
       getUmiAppData();
     }
   });
 
   api.onDevCompileDone(() => {
+    // 用户项目，获取当前项目 appData
     localUmiAppData.update((appData: IAppData) => ({
       ...appData,
       [api.appData.cwd]: api.appData,
