67b94386d6ef65c0f3b5d4738f0d475e3f2ad171
lijiawei <ljw@live.jp>
2023-03-10 21:04:28 +0800

    feat: refresh app-data

--- packages/ui/src/pages/index/index.tsx
+++ packages/ui/src/pages/index/index.tsx
@@ -1,19 +1,27 @@
 import { useEffect, useState } from 'react';
+// @ts-ignore
 import { socket } from 'umi';
 
 function HomePage() {
   const [appData, setAppData] = useState({});
 
-  useEffect(() => {
+  // 请求最新应用信息 appData
+  const refreshAppData = () => {
     socket.send(
       JSON.stringify({
         type: 'app-data',
       }),
     );
+  };
+
+  useEffect(() => {
+    // 获取 appData
+    refreshAppData();
     // 支持卸载
     return socket.listen(({ type, payload }) => {
       console.log(type, payload);
       switch (type) {
+        // 得到最新应用信息 appData
         case 'app-data':
           setAppData(payload);
           break;
@@ -40,6 +48,11 @@ function HomePage() {
       {Object.keys(appData).map((path) => (
         <div key={path}>{path}</div>
       ))}
+      {Object.keys(appData).length > 0 && (
+        <div>
+          <button type='submit' onClick={refreshAppData}>refresh</button>
+        </div>
+      )}
     </div>
   );
 }
