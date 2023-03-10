18376539d20675a53f660646ae1e85135863a043
xiaohuoni <xiaohuoni@dingtalk.com>
2023-03-10 16:35:26 +0800

    fix: framework name error

--- packages/plugin-mongchhi/src/index.ts
+++ packages/plugin-mongchhi/src/index.ts
@@ -3,18 +3,19 @@ import { localUmiAppData, type IAppData } from '@mongchhi/utils';
 import getUmiAppData from './getUmiAppData';
 
 export default (api: IApi) => {
-  if (api.appData.umi.name === 'mongchhi') {
-    api.onStart(() => {
+  api.onStart(() => {
+    if (api.appData.umi.name === 'mongchhi') {
       // mongchhi 主程序，回去全部 appData
       getUmiAppData();
-    });
-  } else {
-    api.onDevCompileDone(() => {
+    }
+  });
+  api.onDevCompileDone(() => {
+    if (api.appData.umi.name !== 'mongchhi') {
       // 用户项目，获取当前项目 appData
       localUmiAppData.update((appData: IAppData) => ({
         ...appData,
         [api.appData.cwd]: api.appData,
       }));
-    });
-  }
+    }
+  });
 };
