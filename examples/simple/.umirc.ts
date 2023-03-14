1d600defcde685571b637297c742191a412d3144
xiaohuoni <xiaohuoni@dingtalk.com>
2023-03-14 18:41:07 +0800

    feat: dnd-kit

--- examples/simple/.umirc.ts
+++ examples/simple/.umirc.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'umi';
 
 export default defineConfig({
   presets: [require.resolve('@mongchhi/preset-mongchhi')],
-  plugins: [require.resolve('@mongchhi/plugin-inject-ui')],
   routes: [
     { path: '/', component: 'index' },
     { path: '/docs', component: 'docs' },
--- packages/preset-mongchhi/src/index.ts
+++ packages/preset-mongchhi/src/index.ts
@@ -2,6 +2,7 @@ export default function () {
   let plugins = [
     require.resolve('@mongchhi/plugin-sdk'),
     require.resolve('@mongchhi/plugin-mongchhi'),
+    require.resolve('@mongchhi/plugin-inject-ui'),
     require.resolve('@mongchhi/plugin-socket'),
     require.resolve('@mongchhi/plugin-antd-theme'),
   ];
